d7d38dcb7fdd131099015bf3e64513c9479d7b3b
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2025-03-02 02:58:29 +0000

    add google Oauth

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,16 +4,21 @@ import './App.css'
 import Register from './Auth/Register';
 import Login from './Auth/login';
 import Dashboard from './pages/Dashboard';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const App = () => {
 
-  return <Router>
-    <Routes>
-      <Route path='/' element = {<Register/>} />
-      <Route path='/login' element = {<Login/>} />
-      <Route path='/dashboard' element = {<Dashboard/>} />
-    </Routes>
-  </Router>
+  return (
+    <GoogleOAuthProvider clientId="1731328532-qjuttrrcijnir9q3be2h22llkajm6oqu.apps.googleusercontent.com">
+      <Router>
+        <Routes>
+          <Route path='/' element = {<Register/>} />
+          <Route path='/login' element = {<Login/>} />
+          <Route path='/dashboard' element = {<Dashboard/>} />
+        </Routes>
+      </Router>
+    </GoogleOAuthProvider>
+  )
 }
 
 export default App
--- front/vite.config.js
+++ front/vite.config.js
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
